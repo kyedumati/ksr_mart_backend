2dd2e581963d33f17ab315c06b3357275f48a182
kyedumati <kasi.yedumati@gmail.com>
2026-09-23 09:09:57 +0530

    TL:login_ehnamcnents

--- login/login_test.py
+++ login/login_test.py
@@ -1,1 +1,7 @@
 print("Im inside login page")
+
+def login():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if username == "admin" and password == "Admin@123":
+        print("login successful")
